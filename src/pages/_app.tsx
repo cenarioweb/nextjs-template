d1a44f2be3da2db3d5f91c19f2e1ac80861b84c9
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-05-18 14:36:20 -0300

    Adding Chakra UI to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import '../../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
