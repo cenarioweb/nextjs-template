5ad5e42dbbdc79109af85b62385989bdcafac31c
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-05-19 12:05:33 -0300

    Adding a button to switch color mode (light/dark)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image';
 import {
   Box,
+  chakra,
   Code,
   Flex,
   Grid,
   GridItem,
   Heading,
   Icon,
+  IconButton,
   Text,
+  Tooltip,
+  useColorMode,
 } from '@chakra-ui/react';
 import { BsArrowRightShort } from 'react-icons/bs';
+import { FiMoon, FiSun } from 'react-icons/fi';
+import Head from 'next/head';
 
 interface HomeCardProps {
   title: string;
@@ -17,17 +23,39 @@ interface HomeCardProps {
   url: string;
 }
 
+function SwitchColorModeButton(): JSX.Element {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Tooltip
+      hasArrow
+      shouldWrapChildren
+      label={colorMode === 'light' ? 'Dark mode' : 'Light mode'}
+      placement="left"
+    >
+      <IconButton
+        onClick={toggleColorMode}
+        aria-label={colorMode === 'light' ? 'Dark mode' : 'Light mode'}
+        icon={colorMode === 'light' ? <FiMoon /> : <FiSun />}
+        variant="ghost"
+      />
+    </Tooltip>
+  );
+}
+
 function HomeCard({ title, description, url }: HomeCardProps) {
+  const { colorMode } = useColorMode();
+
   return (
     <GridItem
-      bgColor="white"
+      bgColor={colorMode === 'light' ? 'white' : 'gray.800'}
       as="a"
       display="block"
       href={url}
       target="_blank"
       borderWidth="1px"
       borderStyle="solid"
-      borderColor="gray.300"
+      borderColor={colorMode === 'light' ? 'gray.300' : 'gray.600'}
       borderRadius={4}
       padding={6}
       transition="all 0.2s ease"
@@ -57,114 +85,152 @@ function HomeCard({ title, description, url }: HomeCardProps) {
 }
 
 export default function Home(): JSX.Element {
+  const { colorMode } = useColorMode();
+
   return (
-    <Flex direction="column" w="100vw" h="100vh">
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
       <Flex
-        className="1"
         direction="column"
-        flex="1"
-        alignItems="center"
-        justifyContent="center"
-        width="100%"
-        maxW="820px"
-        margin="0 auto"
-        p={{ base: '1rem', md: '0' }}
+        w="100vw"
+        h="100vh"
+        bgColor={colorMode === 'light' ? 'gray.50' : 'gray.700'}
+        position="relative"
       >
+        <Box position="absolute" top="1rem" right="1rem">
+          <SwitchColorModeButton />
+        </Box>
         <Flex
-          as="head"
-          direction={{ base: 'column-reverse', md: 'row' }}
-          align="center"
-          justify="space-between"
-          w="100%"
-          p="2rem"
-        >
-          <Box>
-            <Heading
-              as="h1"
-              fontSize="4xl"
-              textAlign={{ base: 'center', md: 'left' }}
-            >
-              Welcome to{' '}
-              <a href="http://www.nextjs.org" target="_blank" rel="noreferrer">
-                Next.js!
-              </a>
-            </Heading>
-            <Heading
-              as="h3"
-              fontSize="lg"
-              marginTop="0.5rem"
-              textAlign={{ base: 'center', md: 'left' }}
-            >
-              With Typescript, ESLint,Prettier, Chakra UI and React Icons.
-            </Heading>
-            <Text
-              fontSize="xl"
-              marginTop="1rem"
-              textAlign={{ base: 'center', md: 'left' }}
-            >
-              Get started by editing{' '}
-              <Code bgColor="gray.200" fontSize="large">
-                pages/index.js
-              </Code>
-            </Text>
-          </Box>
-
-          <Box>
-            <Image
-              src="/man-with-laptop.svg"
-              alt="Man with laptop"
-              width={209}
-              height={160}
-            />
-          </Box>
-        </Flex>
-
-        <Grid
-          as="main"
-          marginTop="4rem"
-          templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }}
-          gap={{ base: 4, md: 8 }}
-        >
-          <HomeCard
-            title="Documentation"
-            description="Find in-depth information about Next.js features and API."
-            url="https://nextjs.org/docs"
-          />
-          <HomeCard
-            title="Learn"
-            description="Learn about Next.js in an interactive course with quizzes!"
-            url="https://nextjs.org/learn"
-          />
-          <HomeCard
-            title="Examples"
-            description="Discover and deploy boilerplate example Next.js projects."
-            url="https://github.com/vercel/next.js/tree/master/examples"
-          />
-          <HomeCard
-            title="Deploy"
-            description="Instantly deploy your Next.js site to a public URL with Vercel."
-            url="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          />
-        </Grid>
-      </Flex>
-
-      <Flex as="footer" borderTop="1px solid #dfdfdf" p="2rem">
-        <Flex
-          as="a"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          width="100%"
-          direction="row"
+          className="1"
+          direction="column"
+          flex="1"
           alignItems="center"
           justifyContent="center"
+          width="100%"
+          maxW="820px"
+          margin="0 auto"
+          p={{ base: '1rem', md: '0' }}
         >
-          <Box lineHeight="24px">Powered by</Box>
-          <Box ml="0.5rem" h={4}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </Box>
+          <Flex
+            as="head"
+            direction={{ base: 'column-reverse', md: 'row' }}
+            align="center"
+            justify="space-between"
+            w="100%"
+            p="2rem"
+          >
+            <Box>
+              <Heading
+                as="h1"
+                fontSize="4xl"
+                textAlign={{ base: 'center', md: 'left' }}
+              >
+                Welcome to{' '}
+                <chakra.a
+                  href="http://www.nextjs.org"
+                  target="_blank"
+                  rel="noreferrer"
+                  color="blue.400"
+                  _hover={{ color: 'blue.600' }}
+                >
+                  Next.js!
+                </chakra.a>
+              </Heading>
+              <Heading
+                as="h3"
+                fontSize="lg"
+                marginTop="0.5rem"
+                textAlign={{ base: 'center', md: 'left' }}
+              >
+                With Typescript, ESLint,Prettier, Chakra UI and React Icons.
+              </Heading>
+              <Text
+                fontSize="xl"
+                marginTop="1rem"
+                textAlign={{ base: 'center', md: 'left' }}
+              >
+                Get started by editing{' '}
+                <Code
+                  bgColor={colorMode === 'light' ? 'gray.200' : 'gray.800'}
+                  fontSize="large"
+                >
+                  pages/index.js
+                </Code>
+              </Text>
+            </Box>
+
+            <Box>
+              <Image
+                src="/man-with-laptop.svg"
+                alt="Man with laptop"
+                width={209}
+                height={160}
+              />
+            </Box>
+          </Flex>
+
+          <Grid
+            as="main"
+            marginTop="4rem"
+            templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }}
+            gap={{ base: 4, md: 8 }}
+          >
+            <HomeCard
+              title="Documentation"
+              description="Find in-depth information about Next.js features and API."
+              url="https://nextjs.org/docs"
+            />
+            <HomeCard
+              title="Learn"
+              description="Learn about Next.js in an interactive course with quizzes!"
+              url="https://nextjs.org/learn"
+            />
+            <HomeCard
+              title="Examples"
+              description="Discover and deploy boilerplate example Next.js projects."
+              url="https://github.com/vercel/next.js/tree/master/examples"
+            />
+            <HomeCard
+              title="Deploy"
+              description="Instantly deploy your Next.js site to a public URL with Vercel."
+              url="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            />
+          </Grid>
+        </Flex>
+
+        <Flex
+          as="footer"
+          borderWidth="1px"
+          borderStyle="solid"
+          borderColor={colorMode === 'light' ? 'gray.200' : 'gray.700'}
+          p="2rem"
+        >
+          <Flex
+            as="a"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            width="100%"
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Box lineHeight="24px">Powered by</Box>
+            <Box ml="0.5rem" h={4}>
+              <Image
+                src={
+                  colorMode === 'light' ? '/vercel.svg' : '/vercel-darkmode.svg'
+                }
+                alt="Vercel Logo"
+                width={72}
+                height={16}
+              />
+            </Box>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
